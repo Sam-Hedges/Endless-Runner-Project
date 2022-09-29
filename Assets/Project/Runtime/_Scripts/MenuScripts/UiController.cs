c6e3a55314ca24aac84c313a940d939f86ad7aeb
Luke Scott <scott_1990@live.co.uk>
2022-09-29 12:23:42 +0100

    Pause. Options and code check for menus
    Done some work with code and being able to change scenes currently working on the pause menu and functions for that. I have done some work in options and main menu to go from each one using buttons

---
+++ Assets/Project/Runtime/_Scripts/MenuScripts/UiController.cs
@@ -0,0 +1,43 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UIElements;
+
+namespace Project.Runtime._Scripts.MenuScripts
+{
+    public class UiController : MonoBehaviour
+    {
+        public Button startButton;
+        public Button optionsButton;
+        public Button quitGame;
+        
+        // Start is called before the first frame update
+        void Start()
+        {
+            var root = GetComponent<UIDocument>().rootVisualElement;
+
+            startButton = root.Q<Button>("Start-button");
+            optionsButton = root.Q<Button>("Options-button");
+            quitGame = root.Q<Button>("Quit-button");
+
+            startButton.clicked += StartButtonPressed;
+            quitGame.clicked += QuitGameButtonPressed;
+            optionsButton.clicked += OptionsButtonPressed;
+        }
+
+        void StartButtonPressed()
+        {
+            SceneManager.LoadScene("SampleScene");
+        }
+
+        void OptionsButtonPressed()
+        {
+            SceneManager.LoadScene("Options");
+        }
+
+        void QuitGameButtonPressed()
+        {
+            Application.Quit();
+            Debug.Log("Game would close");
+        }
+    }
+}
